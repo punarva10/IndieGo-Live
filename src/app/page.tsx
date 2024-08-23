b64f5df22f0b2dbc91726361e4780892f000bcec
Punarv Dinakar <tranquilblue91@gmail.com>
2024-08-24 02:02:31 +0530

    Cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-y-4">
       <h1>Dashboard</h1>
-      <UserButton>Hello</UserButton>
+      <UserButton />
     </div>
   );
 }
