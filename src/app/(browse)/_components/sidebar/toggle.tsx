d9e9f0f9ab94fc165d9f2a747eb3afbc963f8a89
Punarv Dinakar <tranquilblue91@gmail.com>
2024-08-30 17:03:52 +0530

    Added Zustand store

---
+++ src/app/(browse)/_components/sidebar/toggle.tsx
@@ -0,0 +1,5 @@
+export const Toggle = () => {
+    return (<div>
+        Toggle !
+    </div>)
+}
--- src/app/(browse)/_components/sidebar/wrapper.tsx
+++ src/app/(browse)/_components/sidebar/wrapper.tsx
@@ -1,7 +1,22 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { useSidebar } from "@/store/use-sidebar";
+
 type WrapperProps = {
   children: React.ReactNode;
 };
 
 export const Wrapper = ({ children }: WrapperProps) => {
-  return <aside className="fixed left-0 flex flex-col w-60 h-full bg-background border-r border-[#2D2E35] z-50">{children}</aside>;
+  const { collapsed } = useSidebar((state) => state);
+  return (
+    <aside
+      className={cn(
+        "fixed left-0 flex flex-col w-60 h-full bg-background border-r border-[#2D2E35] z-50",
+        collapsed && "w-[70px]"
+      )}
+    >
+      {children}
+    </aside>
+  );
 };
