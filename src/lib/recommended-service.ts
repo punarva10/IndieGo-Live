d742c0b8b08d6186499b2524063b6a1290791e04
Punarv Dinakar <tranquilblue91@gmail.com>
2024-09-02 00:19:22 +0530

    Removed self out of recommended users

--- src/lib/recommended-service.ts
+++ src/lib/recommended-service.ts
@@ -2,16 +2,50 @@ import { db } from "@/lib/db";
 import { getSelf } from "@/lib/auth-service";
 
 export const getRecommended = async () => {
-  try {
-    const users = await db.user.findMany({
-      orderBy: {
-        createdAt: "desc",
-      },
-    });
+  let userId;
 
-    return users;
-  } catch (error) {
-    console.error("Error fetching recommended users from database: ", error);
-    throw new Error("Authentication failed");
+  //get the current user details
+  try {
+    const self = await getSelf();
+    userId = self.id;
+  } catch {
+    userId = null;
+  }
+
+  let users = [];
+
+  if (userId) {
+    //For signed in user
+    try {
+      users = await db.user.findMany({
+        where: {
+          NOT: {
+            id: userId,
+          },
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+
+      return users
+    } catch (error) {
+      console.error("Error fetching recommended users from database: ", error);
+      throw new Error("Authentication failed");
+    }
+  } else {
+    //For signed out user
+    try {
+      const users = await db.user.findMany({
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+
+      return users;
+    } catch (error) {
+      console.error("Error fetching recommended users from database: ", error);
+      throw new Error("Authentication failed");
+    }
   }
 };
