ef7d70abddfd5bfac31303ab4f96b8f84ff17861
Punarv Dinakar <tranquilblue91@gmail.com>
2024-09-06 23:58:16 +0530

    Added base User page

---
+++ src/app/(browse)/[username]/page.tsx
@@ -0,0 +1,11 @@
+type UserPageProps = {
+  params: {
+    username: string;
+  };
+};
+
+const UserPage = ({ params }: UserPageProps) => {
+  return <div> User: {params.username}</div>;
+};
+
+export default UserPage;
