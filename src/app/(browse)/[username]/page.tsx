3d6c482f5ce078d2d3ed1555623afd5bd33ad1bd
Punarv Dinakar <tranquilblue91@gmail.com>
2024-09-08 21:58:47 +0530

    Updated user page

--- src/app/(browse)/[username]/page.tsx
+++ src/app/(browse)/[username]/page.tsx
@@ -1,11 +1,30 @@
+import { isFollowingUser } from "@/lib/follow-service";
+import { getUserByUsername } from "@/lib/user-service";
+import { notFound } from "next/navigation";
+
 type UserPageProps = {
   params: {
     username: string;
   };
 };
 
-const UserPage = ({ params }: UserPageProps) => {
-  return <div> User: {params.username}</div>;
+const UserPage = async ({ params }: UserPageProps) => {
+  const user = await getUserByUsername(params.username);
+
+  if (!user) {
+    notFound();
+  }
+
+  const isFollowing = await isFollowingUser(user.id);
+
+  return (
+    <div className="flex flex-col gap-y-4">
+      {" "}
+      <p>User: {user.username}</p>
+      <p>User: {user.id}</p>
+      <p>is following: {`${isFollowing}`}</p>
+    </div>
+  );
 };
 
 export default UserPage;
